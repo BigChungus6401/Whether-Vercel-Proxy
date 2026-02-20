d2ce94318681cd18ca65179a59c39d670c81dc0d
Bence Nagy <122285506+BigChungus6401@users.noreply.github.com>
2026-02-19 23:09:26 -0500

    Create datasets.js

---
+++ api/datasets.js
@@ -0,0 +1,25 @@
+// Cool Chat code
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets",
+      {
+        headers: {
+          token: process.env.NOAA_TOKEN
+        }
+      }
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        error: "NOAA API error"
+      });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Proxy failure" });
+  }
+}
