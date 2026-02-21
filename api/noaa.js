520a7e5458c4cd180bac943f8e24a4d879a22efc
Bence Nagy <122285506+BigChungus6401@users.noreply.github.com>
2026-02-20 23:45:30 -0500

    New (new) update (updated) New!!!

---
+++ api/noaa.js
@@ -0,0 +1,23 @@
+// Cooler Chat Code
+export default async function handler(req, res) {
+  const { endpoint, ...params } = req.query;
+
+  if (!endpoint) {
+    return res.status(400).json({ error: "Missing endpoint" });
+  }
+
+  const query = new URLSearchParams(params).toString();
+
+  const url = `https://www.ncei.noaa.gov/cdo-web/api/v2/${endpoint}?${query}`;
+
+  const response = await fetch(url, {
+    headers: {
+      token: process.env.NOAA_TOKEN
+    }
+  });
+
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
+}
