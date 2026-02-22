71c5a232030a822ed3a1ec9e2956819d3599ca71
Bence Nagy <122285506+BigChungus6401@users.noreply.github.com>
2026-02-22 01:40:02 -0500

    No cache?

--- api/noaa.js
+++ api/noaa.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
   }
 
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-cache");
   //res.status(200).json(data);
   res.status(200);
   res.send(data);
